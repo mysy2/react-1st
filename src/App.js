0039018de1aad23283f79627d4979e1f1d9f805b
mysy2 <nobalp@gmail.com>
2020-07-27 13:59:23 +0900

    1111

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Subject from "./components/Subject";
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mode: "welcome",
+            welcome: { title: "Welcome", desc: "Hello, React!!" },
+            subject: { title: "WEB", sub: "World Wide Web!" },
+            contents: [
+                { id: 1, title: "HTML", desc: "HTML is for information" },
+                { id: 2, title: "CSS", desc: "CSS is for design" },
+                { id: 3, title: "JavaScript", desc: "JavaScript is for interactive" }
+            ]
+        };
+    }
+    render() {
+        let _title,
+            _desc = null;
+        if (this.state.mode === "welcome") {
+            _title = this.state.welcome.title;
+            _desc = this.state.welcome.desc;
+        } else if (this.state.mode === "read") {
+            _title = this.state.contens[0].title;
+            _desc = this.state.contens[0].desc;
+        }
+        return (
+            <div className="App">
+                <Subject
+                    title={this.state.subject.title}
+                    sub={this.state.subject.sub}
+                    onChangePage={function () {
+                        this.setState({ mode: "welcome" });
+                    }.bind(this)}
+                ></Subject>
+                <TOC data={this.state.contents}></TOC>
+                <Content title={_title} desc={_desc}></Content>
+            </div>
+        );
+    }
 }
 
 export default App;

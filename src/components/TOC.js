3d540f27b1098a4ebdb23c7f2af0eb0c38e367dc
mysy2 <nobalp@gmail.com>
2020-08-01 00:28:41 +0900

    0731

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 
 class TOC extends Component {
+    shouldComponentUpdate(newProps, newState) {
+        if (this.props.data === newProps.data) {
+            return false;
+        }
+        return true;
+    }
     render() {
         let lists = [];
         let data = this.props.data;
